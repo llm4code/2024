a7b8c18a56b24211d6cae2a864e61414999c9064
Yuxiang Wei <yuxiang630.wei@gmail.com>
2024-02-27 21:43:57 -0600

    fix: order

--- utils/parse_accepted_papers.py
+++ utils/parse_accepted_papers.py
@@ -11,7 +11,7 @@ df = pd.json_normalize(data)
 
 print("Number of papers:", len(df))
 # Extract the title and author names
-# If its a positional paper (indicated by column "paper_type"), add a label "[Position Paper]" to the title
+# If it's a positional paper (indicated by column "paper_type"), add a label "[Position Paper]" to the title
 df["title"] = df.apply(
     lambda row: (
         f"{row['title']} 💡"
@@ -21,12 +21,14 @@ df["title"] = df.apply(
     axis=1,
 )
 df["authors"] = df["authors"].apply(
-    lambda x:  ", ".join([f"<span style=\"white-space: nowrap;\">{a['first']}&nbsp;{a['last']}</span>" for a in x]) 
+    lambda x: ", ".join([f"<span style=\"white-space: nowrap;\">{a['first']}&nbsp;{a['last']}</span>" for a in x]) 
 )
 
-# print(df[['title', 'authors']].head())
-# Add a column for position papers
-# df['position_paper'] = df['title'].apply(lambda x: 'Yes' if 'Position' in x else 'No')
+# Determine if it's a position paper
+df["is_position_paper"] = df["paper_type"].apply(lambda x: "position" in x.lower())
+
+# Sort the DataFrame: first by 'is_position_paper' to push position papers to the bottom, then alphabetically by 'title'
+df = df.sort_values(by=["is_position_paper", "title"], ascending=[True, True])
 
 # Print the DataFrame as a Markdown table
 print(df[["title", "authors"]].to_markdown(index=False))
